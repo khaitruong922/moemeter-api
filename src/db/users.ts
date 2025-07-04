c41681f6661eea2c46d76f9fd1c493053abd41a6
Khai Truong <khai.truong@codelink.io>
2025-07-04 16:20:54 +0700

    Order by pages read after books read

--- src/db/users.ts
+++ src/db/users.ts
@@ -5,7 +5,8 @@ export const selectAllUsers = async (sql: postgres.Sql<{}>): Promise<User[]> => 
 	const rows = await sql<User[]>`
     SELECT id, name, avatar_url, books_read, pages_read, sync_status
     FROM users
-    ORDER BY books_read DESC NULLS LAST
+    ORDER BY books_read DESC, pages_read DESC
+    WHERE books_read IS NOT NULL
   `;
 
 	return rows;
