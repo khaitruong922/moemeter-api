ed9b9bc1d0fbeafc869bbb76c49f7763e0451773
Khai Truong <khai.truong@codelink.io>
2025-12-12 10:32:23 +0700

    Remove test files

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import app from "../src"
-
-describe('GET /', () => {
-  it('Should return 200 response', async () => {
-    const res = await app.request('http://localhost/');
-    expect(res.status).toBe(200);
-  });
-});
