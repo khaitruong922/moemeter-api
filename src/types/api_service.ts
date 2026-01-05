8530ffda9b70998b02c90418b703978594d3e60a
Khai Truong <khai.truong@codelink.io>
2026-01-06 02:45:57 +0700

    Refactor

---
+++ src/types/api_service.ts
@@ -0,0 +1,24 @@
+type UserReadData = {
+	id: number;
+	book_id: number;
+	title: string;
+	author: string;
+	author_url: string;
+	thumbnail_url: string;
+	page: number;
+	date: Date | null;
+};
+type FetchUserReadsOfPagesResponse = {
+	reads: UserReadData[];
+	read_pages_count: number;
+};
+
+export interface ApiService {
+	fetchUserReadsOfPages(
+		id: number,
+		pageStart: number,
+		pageEnd: number,
+		perPage: number,
+		bookcase: string | null
+	): Promise<FetchUserReadsOfPagesResponse>;
+}
--- src/types/app_env.ts
+++ src/types/app_env.ts
@@ -1,29 +1,4 @@
-// Copied from bookmeter-api
-type UserReadData = {
-	id: number;
-	book_id: number;
-	title: string;
-	author: string;
-	author_url: string;
-	thumbnail_url: string;
-	page: number;
-	date: Date | null;
-};
-type FetchUserReadsOfPagesResponse = {
-	reads: UserReadData[];
-	read_pages_count: number;
-};
-//
-
-export interface ApiService {
-	fetchUserReadsOfPages(
-		id: number,
-		pageStart: number,
-		pageEnd: number,
-		perPage: number,
-		bookcase: string | null
-	): Promise<FetchUserReadsOfPagesResponse>;
-}
+import { ApiService } from './api_service';
 
 export type AppEnv = {
 	DATABASE_URL: string;
