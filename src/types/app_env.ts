5e53cbbcf4100811806718fcc986ad33c2ec4296
Khai Truong <khai.truong@codelink.io>
2026-01-06 02:38:06 +0700

    Typing

--- src/types/app_env.ts
+++ src/types/app_env.ts
@@ -1,3 +1,20 @@
+// Copied from bookmeter-api
+type UserReadData = {
+	id: number;
+	book_id: number;
+	title: string;
+	author: string;
+	author_url: string;
+	thumbnail_url: string;
+	page: number;
+	date: Date | null;
+};
+type FetchUserReadsOfPagesResponse = {
+	reads: UserReadData[];
+	read_pages_count: number;
+};
+//
+
 export interface ApiService {
 	fetchUserReadsOfPages(
 		id: number,
@@ -5,7 +22,7 @@ export interface ApiService {
 		pageEnd: number,
 		perPage: number,
 		bookcase: string | null
-	): Promise<any>;
+	): Promise<FetchUserReadsOfPagesResponse>;
 }
 
 export type AppEnv = {
